4418f891049182708922644686414b87a63823b0
Fernanda Guimaraes <fernandagfsnet@gmail.com>
2023-02-26 17:21:19 -0300

    iniciando java

---
+++ src/PrincipalGato.java
@@ -0,0 +1,8 @@
+public class PrincipalGato {
+
+
+    public static void main(String[] args) {
+        Gato gato = new Gato("chiquinha", "preto com branoo", 2);
+        System.out.println(gato);
+    }
+}
